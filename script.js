0e5d0ebed3c066bd8b2c7ba86d1f22b962fbfdfb
Jason Greenwald <34841215+jaycgreenwald@users.noreply.github.com>
2022-07-05 22:04:21 -0700

    7/5/2022 - FEM feedback fixes
    Add aria-label, js- class names, updates to javascript

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-// prevent form from being submitted
-document.getElementById("submit").addEventListener("click", function(event){
+const submitButton = document.querySelector(".js-btn-rounded");
+
+// prevent form from being submitted before validation
+submitButton.addEventListener("click", function(event) {
     event.preventDefault()
   });
 
-document.getElementById("submit").addEventListener("click", namedFunction);
 
-function namedFunction() { 
-    console.log("test");
-};
+const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+
+submitButton.addEventListener("click", validateEmail);
+
+submitButton.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    submitButton.click();
+  }
+});
+
+function validateEmail() {
+  const inputEmail = document.querySelector('.js-input-email-rounded').value;
+  const inputField = document.querySelector('.js-input-email-rounded');
+  const isValidEmail = emailRegex.test(inputEmail);
+  
+  if (isValidEmail === false) {
+      inputField.style.border = "1px solid hsl(0, 93%, 68%)";
+      document.querySelector('.js-error-email').style.display = 'block';
+    } else {
+      inputField.style.border = "1px solid hsl(0, 36%, 70%)";
+      document.querySelector('.js-error-email').style.display = 'none';
+  }
+  
+}
